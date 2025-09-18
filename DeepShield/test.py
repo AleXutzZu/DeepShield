37e7089c4c35f8e7e0aa51648641228d86c5991c
Alex David <davidalexrobertyt@gmail.com>
2025-09-18 15:01:01 +0300

    Rename folder

--- DeepShield/test.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-url = "http://localhost:8000/detect-deepfake/"
-
-# Send local file
-with open("images/test1.png", "rb") as f:
-    response = requests.post(url, files={"file": f})
-
-print(response.json())
